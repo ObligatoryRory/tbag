536b3a41dcc88989f7d551010f1db65dd3d1cc2e
frankcash <cfcash@mtu.edu>
2013-12-07 02:15:51 -0500

    added two monsters

--- src/MonsterTable.java
+++ src/MonsterTable.java
@@ -14,6 +14,13 @@ public class MonsterTable {
 		aGreenSlimeItems.add(itemTable.getItem("small_health_potion"));
 		monsterMap.put("green_slime", new Monster(30, "Green Slime", "A basic, worthless slime, kill it with fire!", 5, aGreenSlimeItems));
 		
+		ArrayList<Item>aGoblinShredderItems = new ArrayList<Item>(); //heavy goblin unit
+		aGoblinShredderItems.add(itemTable.getItem("iron_shortsword"));
+		monsterMap.put("goblin_shredder", new Monster(90, "Goblin Shredder","A goblin wearing equipment, ready for battle.", 22, aGoblinShredderItems));
+		
+		ArrayList<Item>aRizzarkItems = new ArrayList<Item>(); //goblin boss
+		aRizzarkItems.add(itemTable.getItem(""));
+		monsterMap.put("rizzark", new Monster(120, "Rizzark", "A gobling boss!", 28, aRizzarkItems));
 	}
 }
 
