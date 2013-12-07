f887b27311ebf2bd453f63d32df3473554d2cd24
Rory Straubel <rmstraub@mtu.edu>
2013-12-07 02:05:10 -0500

    ItemList Update

---
+++ src/Events_Rory.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class Events_Rory extends Event {
+
+	@Override
+	public void giveStat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Item> giveReward() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
